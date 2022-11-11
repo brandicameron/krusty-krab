a6a055dc0ff3ad37b848d869b78e619338436cba
Brandi Cameron <hello@brandicameron.com>
2022-11-11 15:12:36 -0600

    Set language in next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+};
