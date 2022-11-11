ae041eaebd22835560ac2982e692b7b520dcd26f
Brandi Cameron <hello@brandicameron.com>
2022-11-11 10:06:18 -0600

    Created separate data file for menu items

--- components/index/Menu.js
+++ components/index/Menu.js
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import { AppContext } from '../../AppContext';
 import Image from 'next/image';
 import KelpShake from '../../public/images/kelp-shake.png';
+import { menuItems } from '../../data/menuItems';
 import { useAddToCart } from '../../hooks/useAddToCart';
 
 export default function Menu() {
@@ -67,62 +68,3 @@ export default function Menu() {
     </section>
   );
 }
-
-const menuItems = [
-  {
-    name: 'Krabby Patty',
-    price: '1.25',
-  },
-  {
-    name: 'Double Krabby Patty',
-    price: '2.00',
-  },
-  {
-    name: 'Tripple Krabby Patty',
-    price: '3.00',
-  },
-  {
-    name: 'Krabby Meal',
-    price: '3.50',
-  },
-  {
-    name: 'Double Krabby Meal',
-    price: '3.75',
-  },
-  {
-    name: 'Triple Krabby Meal',
-    price: '4.00',
-  },
-  {
-    name: 'Salty Sea Dog',
-    price: '1.25',
-  },
-  {
-    name: 'Footlong',
-    price: '2.00',
-  },
-  {
-    name: 'Sailors Surprise',
-    price: '3.00',
-  },
-  {
-    name: 'Golden Loaf',
-    price: '2.00',
-  },
-  {
-    name: 'Coral Bits',
-    price: '1.50',
-  },
-  {
-    name: 'Kelp Rings',
-    price: '1.50',
-  },
-  {
-    name: 'Seafoam Soda',
-    price: '1.25',
-  },
-  {
-    name: 'Kelp Shake',
-    price: '2.00',
-  },
-];
---
+++ data/menuItems.js
@@ -0,0 +1,58 @@
+export const menuItems = [
+  {
+    name: 'Krabby Patty',
+    price: '1.25',
+  },
+  {
+    name: 'Double Krabby Patty',
+    price: '2.00',
+  },
+  {
+    name: 'Tripple Krabby Patty',
+    price: '3.00',
+  },
+  {
+    name: 'Krabby Meal',
+    price: '3.50',
+  },
+  {
+    name: 'Double Krabby Meal',
+    price: '3.75',
+  },
+  {
+    name: 'Triple Krabby Meal',
+    price: '4.00',
+  },
+  {
+    name: 'Salty Sea Dog',
+    price: '1.25',
+  },
+  {
+    name: 'Footlong',
+    price: '2.00',
+  },
+  {
+    name: 'Sailors Surprise',
+    price: '3.00',
+  },
+  {
+    name: 'Golden Loaf',
+    price: '2.00',
+  },
+  {
+    name: 'Coral Bits',
+    price: '1.50',
+  },
+  {
+    name: 'Kelp Rings',
+    price: '1.50',
+  },
+  {
+    name: 'Seafoam Soda',
+    price: '1.25',
+  },
+  {
+    name: 'Kelp Shake',
+    price: '2.00',
+  },
+];
