cd68df7aab3310c55c73cd13d3799f3d2cf38c61
Brandi Cameron <hello@brandicameron.com>
2022-11-14 16:21:13 -0600

    Added useEffect to set document height and use that measurement in the bubbles positioning

--- components/shared/Bubbles.js
+++ components/shared/Bubbles.js
@@ -1,14 +1,42 @@
 import styles from '../../styles/shared/Bubbles.module.css';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Bubble from '../../public/images/bubble.svg';
 
 export default function Bubbles() {
+  const [windowHeight, setWindowHeight] = useState(null);
+
+  useEffect(() => {
+    const innerHeight = window.document.body.offsetHeight;
+    setWindowHeight(innerHeight);
+  }, []);
+
   return (
     <div className={styles.bubbles}>
-      <Image className={styles.bubble1} src={Bubble} alt='' />
-      <Image className={styles.bubble2} src={Bubble} alt='' />
-      <Image className={styles.bubble3} src={Bubble} alt='' />
-      <Image className={styles.bubble4} src={Bubble} alt='' />
+      <Image
+        className={styles.bubble1}
+        src={Bubble}
+        alt=''
+        style={{ transform: `translateY(-${windowHeight}px) translateX(0)` }}
+      />
+      <Image
+        className={styles.bubble2}
+        src={Bubble}
+        alt=''
+        style={{ transform: `translateY(-${windowHeight}px) translateX(0)` }}
+      />
+      <Image
+        className={styles.bubble3}
+        src={Bubble}
+        alt=''
+        style={{ transform: `translateY(-${windowHeight}px) translateX(0)` }}
+      />
+      <Image
+        className={styles.bubble4}
+        src={Bubble}
+        alt=''
+        style={{ transform: `translateY(-${windowHeight}px) translateX(0)` }}
+      />
     </div>
   );
 }
