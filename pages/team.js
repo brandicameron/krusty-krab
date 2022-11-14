ad717acc922ea6b3cbeea40187df47c79c9911b2
Brandi Cameron <hello@brandicameron.com>
2022-11-14 13:14:16 -0600

    Added team page

---
+++ pages/team.js
@@ -0,0 +1,9 @@
+import styles from '../styles/team/Team.module.css';
+
+export default function Team() {
+  return (
+    <section className={styles.ourTeam}>
+      <h1>Our Team</h1>
+    </section>
+  );
+}
