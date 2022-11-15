2b50517d23590a73ff3b21a2700ffbafb17940af
Brandi Cameron <hello@brandicameron.com>
2022-11-15 10:08:01 -0600

    Added share graphic meta tags to cart and checkout pages

--- pages/cart.js
+++ pages/cart.js
@@ -11,7 +11,18 @@ export default function Cart() {
     <div className={styles.container}>
       <Head>
         <title>The Krusty Krab | Your Cart</title>
+        <meta
+          name='description'
+          content='Welcome to the Krusty Krab, home of the world famous Krabby Patty and the most popular restaurant in Bikini Bottom!'
+        />
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          property='og:title'
+          content='The Krusty Krab, home of the world famous Krabby Patty!'
+        />
+        <meta property='og:image' content='/share.jpg' />
       </Head>
+
       <section className={styles.cart}>
         <h1 className='display-text'>Cart</h1>
         <ul>
--- pages/checkout.js
+++ pages/checkout.js
@@ -7,6 +7,16 @@ export default function Checkout() {
     <section className={styles.checkout}>
       <Head>
         <title>The Krusty Krab | Checkout</title>
+        <meta
+          name='description'
+          content='Welcome to the Krusty Krab, home of the world famous Krabby Patty and the most popular restaurant in Bikini Bottom!'
+        />
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          property='og:title'
+          content='The Krusty Krab, home of the world famous Krabby Patty!'
+        />
+        <meta property='og:image' content='/share.jpg' />
       </Head>
       <h1>
         Okay, how far are we really going to take this? <span>😆</span>
