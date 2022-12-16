ac780cdcad9bdec5902d13e4fb2bc60eaec49d2d
Brandi Cameron <hello@brandicameron.com>
2022-12-16 12:34:38 -0600

    Created universal Add to Cart button component

--- components/index/MenuItem.js
+++ components/index/MenuItem.js
@@ -1,25 +1,15 @@
 import styles from '../../styles/index/MenuItem.module.css';
-import { useContext } from 'react';
-import { AppContext } from '../../AppContext';
-import { useAddToCart } from '../../hooks/useAddToCart';
 import { motion } from 'framer-motion';
+import AddToCartBtn from '../shared/AddToCartBtn';
 
 export default function MenuItem({ item }) {
-  const { addedToCart, cartItems, setCartItems } = useContext(AppContext);
-  const { handleAddToCart } = useAddToCart(cartItems, setCartItems);
-
   return (
     <li className={styles.menuItem}>
-      <button
-        onClick={handleAddToCart}
-        data-value={item.name}
-        aria-label={`Add ${item.name} to cart for ${item.price} dollars.`}
-        disabled={addedToCart ? true : false}
-      >
+      <AddToCartBtn btnClass='' data={item.name}>
         <span aria-hidden='true'>{item.name}</span>
         <motion.span className={styles.dotLeaders} aria-hidden='true'></motion.span>
         <span aria-hidden='true'>{item.price}</span>
-      </button>
+      </AddToCartBtn>
     </li>
   );
 }
