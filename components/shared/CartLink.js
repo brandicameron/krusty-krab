799cf804190bfe6c43dd864cdb66665af4dbfae4
Brandi Cameron <hello@brandicameron.com>
2022-12-17 12:28:18 -0600

    Updated comment

--- components/shared/CartLink.js
+++ components/shared/CartLink.js
@@ -8,8 +8,6 @@ export default function CartLink() {
   const [newItemAdded, setNewItemAdded] = useState(false);
   const { cartItems } = useContext(AppContext);
 
-  // Try to do this without using useEffect
-
   useEffect(() => {
     let sum = 0;
     cartItems.forEach((value) => {
@@ -18,7 +16,7 @@ export default function CartLink() {
     setNumItemsInCart(sum);
   }, [cartItems]);
 
-  // Makes number bubble above cart animate when new item added
+  // Makes number bubble above cart animate when new item added or removed
   useEffect(() => {
     setNewItemAdded(true);
     let timer = setTimeout(() => {
