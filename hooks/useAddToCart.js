fed33716bf7e4c115d0336a16f766ac9731eac9c
Brandi Cameron <hello@brandicameron.com>
2023-02-14 08:21:56 -0600

    Refactored setCartItems to use ternary instead of if statement with duplicated code

--- hooks/useAddToCart.js
+++ hooks/useAddToCart.js
@@ -22,21 +22,12 @@ export function useAddToCart() {
     const currentItemPrice = e.currentTarget.getAttribute('data-price');
     setItemName(currentItemName);
 
-    if (cartItems.has(currentItemName)) {
-      setCartItems((prev) =>
-        new Map(prev).set(currentItemName, {
-          quantity: cartItems.get(currentItemName).quantity + 1,
-          price: currentItemPrice,
-        })
-      );
-    } else {
-      setCartItems((prev) =>
-        new Map(prev).set(currentItemName, {
-          quantity: 1,
-          price: currentItemPrice,
-        })
-      );
-    }
+    setCartItems((prev) =>
+      new Map(prev).set(currentItemName, {
+        quantity: cartItems.has(currentItemName) ? cartItems.get(currentItemName).quantity + 1 : 1,
+        price: currentItemPrice,
+      })
+    );
 
     setAddedToCart(true);
   };
